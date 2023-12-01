979ba8fc4f14f32aa3dc9b0decbc696cef0438b4
krishnendu0202 <krishnendu.nandy@whitehatjr.com>
2023-12-01 12:46:56 +0530

    inital-push

---
+++ NodeJs_module/my_module.js
@@ -0,0 +1,25 @@
+// Type-1
+function myDateTime(){
+    return Date();
+}
+module.exports = {myDateTime};
+
+
+// Type-2
+exports.myDateTime = function () {
+    return Date();
+};
+
+
+// Type-3
+function myDateTime(){
+    return Date();
+}
+exports.myDateTime = myDateTime();
+
+// Type-4
+function myDateTime(){
+    return Date();
+}
+var date_time   =   myDateTime();
+module.exports = {date_time};
---
+++ myfirst.js
@@ -0,0 +1,10 @@
+var http = require('http'); // to include http module use require // built-in module
+
+var date_time   =   require('./NodeJs_module/my_module')
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("The date and time are currently: " + date_time.date_time);
+  res.end('Hello World from Krishnendu!');
+//   res.end();
+}).listen(8080);
